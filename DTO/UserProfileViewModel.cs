28cacab9f632b6d23cf735f5d55a38797a41fe80
dimazhelovanov <dimazhelovanov@gmail.com>
2025-04-27 14:15:01 +0400

    Авторизация

---
+++ DTO/UserProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProfessionalCardWebClientApp.DTO
+{
+    public class UserProfileViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CurrentProfession { get; set; }
+        public List<string> FavoriteProfessions { get; set; }
+        public List<ProfessionDTO> AvailableProfessions { get; set; }
+        public int SelectedFavoriteProfessionId { get; set; }
+    }
+}
